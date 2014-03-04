94850dd8f19328e013dee819f59d5d18899d025d
Masahito Yoshida <masahito@axsh.net>
2014-03-05 00:23:00 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+vipple testing box
+==================
+
+[vipple](https://github.com/hansode/vipple) testing box.
+
+Getting Started
+---------------
+
+```
+$ vagrant up
+```
---
+++ develop/config.d/base.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+set -x
+
+# functions
+
+## common
+
+function gen_ifcfg_path() {
+  local ifname=${1:-eth0}
+  local ifcfg_path=/etc/sysconfig/network-scripts/ifcfg
+
+  echo ${ifcfg_path}-${ifname}
+}
+
+function install_ifcfg_file() {
+  local ifname=${1:-eth0}
+
+  tee $(gen_ifcfg_path ${ifname}) </dev/stdin
+}
+
+function render_ifcfg_eth() {
+  local ifname=${1:-eth0}
+  
+  cat <<-EOS
+	DEVICE=${ifname}
+	BOOTPROTO=none
+	ONBOOT=yes
+	EOS
+}
+
+function install_vipple() {
+  local deploy_dir=/var/tmp/vipple
+
+  if [[ ! -d ${deploy_dir} ]]; then
+    git clone https://github.com/hansode/vipple ${deploy_dir}
+  fi
+
+ #cd ${deploy_dir}
+ #git pull origin master
+ #install -m 755 vipple /etc/init.d/vipple
+ #chkconfig --add vipple
+
+  if [[ -d /etc/vipple ]]; then
+    mkdir  /etc/vipple
+  fi
+}
+
+#
+
+render_ifcfg_eth eth1 | install_ifcfg_file eth1
+install_vipple
