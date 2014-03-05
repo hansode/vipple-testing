4c63352152316741f718584ab7a40a6415b10d19
Masahito Yoshida <masahito@axsh.net>
2014-03-05 19:47:58 +0900

    split failover-test.sh.

--- vip-failover/failover-test.sh
+++ vip-failover/failover-test.sh
@@ -7,64 +7,9 @@ set -e
 set -o pipefail
 set -x
 
-# functions
+#
 
-## common
-
-function run_in_target() {
-  local node=${1}; shift
-  vagrant ssh ${node} -c "${@}"
-}
-
-function show_ipaddr() {
-  local node=${1}
-  shift; eval local "${@}"
-  run_in_target ${node} "ip addr show ${ifname} | grep -w inet"
-}
-
-## vipple
-
-function force_stop_vipple() {
-  local node=${1}
-  run_in_target ${node} "sudo service vipple stop || :"
-}
-
-function stop_vipple() {
-  local node=${1}
-  run_in_target ${node} "sudo service vipple stop"
-}
-
-function force_start_vipple() {
-  local node=${1}
-  run_in_target ${node} "sudo service vipple start || :"
-}
-
-function start_vipple() {
-  local node=${1}
-  run_in_target ${node} "sudo service vipple start"
-}
-
-## vipple-zero
-
-function force_stop_vipple_zero() {
-  local node=${1}
-  run_in_target ${node} "sudo service vipple-zero stop || :"
-}
-
-function stop_vipple_zero() {
-  local node=${1}
-  run_in_target ${node} "sudo service vipple-zero stop"
-}
-
-function force_start_vipple_zero() {
-  local node=${1}
-  run_in_target ${node} "sudo service vipple-zero start || :"
-}
-
-function start_vipple_zero() {
-  local node=${1}
-  run_in_target ${node} "sudo service vipple-zero start"
-}
+. failover-functions.sh
 
 # setup
 
